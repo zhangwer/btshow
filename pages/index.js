48c2e3f1d4127720e6f85a5713763af848b97ae0
zw <francezw@hotmail.com>
2021-12-23 21:57:21 +0800

    1.0

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-const Home = function ({ allshows}) {
-    const shows=allshows
+const Home = function ({ allshows }) {
+    const shows = allshows
     const [imgindex, setImgindex] = React.useState(0);
     const [x, setX] = React.useState(0);
     const touchstart = function (e) {
         if (e.clientX)
             setX(e.clientX)
         else setX(e.changedTouches[0].clientX)
-        
+
     }
     const touchend = async function (e) {
         if (e.clientX) {
@@ -16,7 +16,7 @@ const Home = function ({ allshows}) {
             }
             else if (e.clientX - x < -10 && imgindex < shows.length - 1) {
                 setImgindex(imgindex + 1)
-               
+
             }
         }
         else {
@@ -28,15 +28,15 @@ const Home = function ({ allshows}) {
                 setImgindex(imgindex + 1)
             }
         }
-      
+
     }
     return (
         < div >
             <div className='btswiper'
                 onMouseDown={touchstart} onTouchStart={touchstart} onTouchEnd={touchend}
                 onMouseUp={touchend} >
-                < div className='box' style={{ backgroundImage: 'url("' + shows[imgindex].imgurl + '")' }}>{imgindex<shows.length-1?<img hidden src={shows[imgindex+1].imgurl}/>:''}</div>
-                <div>{shows[imgindex].title} ({imgindex+1}/{shows.length})</div>
+                < div className='box' style={{ backgroundImage: 'url("' + shows[imgindex].imgurl + '")' }}>{imgindex < shows.length - 1 ? <img hidden src={shows[imgindex + 1].imgurl} /> : ''}</div>
+                <div>{shows[imgindex].title} ({imgindex + 1}/{shows.length})</div>
             </div >
         </div>
     );
@@ -50,8 +50,9 @@ export async function getStaticProps() {
     })
     const allshows = await res.json()
     return {
-      props: { allshows },
+        props: { allshows },
+        revalidate: 1
     }
-  }
+}
 
-  export default Home
+export default Home
