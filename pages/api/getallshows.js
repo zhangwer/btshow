07b642da7b2719139acab77333118953acc30db7
zw <francezw@hotmail.com>
2021-12-24 18:35:03 +0800

    1.0

--- pages/api/getallshows.js
+++ pages/api/getallshows.js
@@ -2,7 +2,7 @@ import { connectToDatabase } from './mongodb';
 module.exports = async (req, res) => {
     const { db } = await connectToDatabase()
     const collection = await db.collection('goods')
-    const result = await collection.find().toArray()
+    const result = await collection.find().sort({"_id":-1}).toArray()
     res.status(200).json(result)
 
     return
